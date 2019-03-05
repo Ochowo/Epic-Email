d01ffb166bb7d2365e51580f1a88bd470945db39
ochowo <ochowoikongbeh@gmail.com>
2019-03-05 13:21:59 +0100

    feat(endpoint): user signup page [delivers #164343246]

---
+++ server/helpers/userExists.js
@@ -0,0 +1,16 @@
+import users from '../db/index';
+
+const userExists = (req, res, next) => {
+  const {
+    email,
+  } = req.body;
+  const result = users.map(user => user.email);
+  if (result.includes(email)) {
+    return res.status(409).json({
+      status: 409,
+      error: `the email ${email} already exists, please choose another email.`,
+    });
+  }
+  return next();
+};
+export default userExists;
---
+++ server/routes/index.js
@@ -0,0 +1,3 @@
+import router from './user';
+
+export default router;
