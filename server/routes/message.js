07c6912fb36081715ff0585e02a5f0b89ae3eadf
ochowo <ochowoikongbeh@gmail.com>
2019-03-07 02:57:59 +0100

    (feat): get all user sent messages ensure user is logged in define endpoints to get sent messages [delivers #164345012]

--- server/routes/message.js
+++ server/routes/message.js
@@ -9,4 +9,5 @@ const router = express.Router();
 router.post('/messages', isLoggedIn, checkMsgInput, messages.newMessage);
 router.get('/messages', isLoggedIn, messages.getAllMessages);
 router.get('/messages/unread', isLoggedIn, messages.getUnread);
+router.get('/messages/sent', isLoggedIn, messages.getSent);
 export default router;
