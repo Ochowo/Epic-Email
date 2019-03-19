c641a92cb13477f41d06d44526966cb58f2fb2e5
ochowo <ochowoikongbeh@gmail.com>
2019-03-19 14:49:50 +0100

    feat(group): implement route to add a user to a group create the endpoint create route validate endpoint [delivers #164557344]

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -8,4 +8,5 @@ router.post('/', isLoggedIn, groups.newGroup);
 router.get('/', isLoggedIn, groups.getGroups);
 router.patch('/:id/name', isLoggedIn, groups.updateGroup);
 router.delete('/:id', isLoggedIn, groups.deleteGroup);
+router.post('/:id/users', isLoggedIn, groups.createUser);
 export default router;
