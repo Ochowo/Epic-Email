549d7a0370192abe1fab339ebfd9afcb602d81f8
ochowo <ochowoikongbeh@gmail.com>
2019-03-19 01:46:16 +0100

    feat(groups): implement user route to update group name creat the endpoint creat the route validate the endpoint [delivers #164554546]

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -6,4 +6,5 @@ const router = express.Router();
 
 router.post('/', isLoggedIn, groups.newGroup);
 router.get('/', isLoggedIn, groups.getGroups);
+router.patch('/:id/name', isLoggedIn, groups.updateGroup);
 export default router;
