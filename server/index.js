f951f1c5631530f3d6219c17156af5576ebc7d2d
ochowo <ochowoikongbeh@gmail.com>
2019-04-03 17:09:31 +0200

    chore(user): consume signup endpoint create script to fetch api add route to entry file modify ui to accomodate changes [finishes #164818130]

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 import 'babel-polyfill';
 import cors from 'cors';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 import { users, messages, groups } from './routes/index';
 
@@ -9,20 +11,33 @@ dotenv.config();
 const app = express();
 
 // Enable CORS
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.options('*', cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  }),
+);
 app.all('/*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
+
 // Parse incoming request data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(morgan('combined'));
 
 
 // Serve ui templates
 app.use(express.static('ui'));
+app.use('/ui', express.static(path.resolve(__dirname, '../../ui/')));
+
 app.use('/api/v1/auth', users);
 app.use('/api/v1', messages);
 app.use('/api/v1/groups', groups);
