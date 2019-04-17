a7ef3aa4112e078573e34a611834e2e83c858069
Ochowo <ochowoikongbeh@gmail.com>
2019-04-17 01:25:13 +0100

    Update groupMessages.js

--- server/models/groupMessages.js
+++ server/models/groupMessages.js
@@ -11,8 +11,8 @@ const grpMsgQuery = async () => {
    FOREIGN KEY(groupId) REFERENCES groups(id) ON DELETE CASCADE)`;
 
   if (process.env.NODE_ENV === 'test') {
-    sqlQuery = `DROP TABLE IF EXISTS messages CASCADE;
-  CREATE TABLE IF NOT EXISTS messages
+    sqlQuery = `DROP TABLE IF EXISTS groupMessages CASCADE;
+  CREATE TABLE IF NOT EXISTS groupMessages
     (id SERIAL NOT NULL PRIMARY KEY, createdOn TIMESTAMPTZ DEFAULT NOW(), subject VARCHAR(255) NOT NULL,
     message VARCHAR(255) NOT NULL, parentMessageId INT, status VARCHAR(255) DEFAULT 'unread' NOT NULL, senderId INT NOT NULL, receiverId INT NOT NULL)`;
   }
