bfd4bf7a7cb1493f8eb0ee857b94a0fd431fa566
unknown <ochowo.ikongbeh@gmail.com>
2020-11-28 00:50:42 +0100

    fix heroku

--- src/config/config.js
+++ src/config/config.js
@@ -19,10 +19,6 @@ module.exports = {
   },
 
   production: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL'
   },
 };
